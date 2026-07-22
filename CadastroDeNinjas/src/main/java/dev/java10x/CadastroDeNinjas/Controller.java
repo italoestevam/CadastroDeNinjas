16f410fb8e39a18d24c14135844d9d970fa02d67
italoestevam <italoestevam21@gmail.com>
2026-07-22 17:20:09 -0300

    first

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,22 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+//import org.springframework.web.bind.annotation.RestController;
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+
+
+
+
+
+}
